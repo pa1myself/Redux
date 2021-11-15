ff36bad2398fb8b607dffca37269952f6a386837
pa1myself <kumarpavam869.com>
2021-11-15 17:00:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
-function App() {
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import store from './store';
+
+//const store = createStore(() => [], {}, applyMiddleware());
+
+class App extends Component  {
+  render(){
   return (
+    <Provider store={store} >
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="app-title">Welcome to React</h1>
       </header>
+      <PostForm />
+      <Posts />
     </div>
+    </Provider>
   );
+  }
 }
 
 export default App;
